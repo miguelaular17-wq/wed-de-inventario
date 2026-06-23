bea698fc183f9f5315eefd31d1b512ee6041d82b
Miguel Aular <miguelaular17@gmail.com>
2026-06-23 17:15:32 -0400

    Fix: Skip SampleProductsSeeder if products already exist in the database to prevent overwriting user data on redeployment

--- database/seeders/SampleProductsSeeder.php
+++ database/seeders/SampleProductsSeeder.php
@@ -9,6 +9,11 @@ class SampleProductsSeeder extends Seeder
 {
     public function run(MultisedeImportService $import): void
     {
+        if (\Illuminate\Support\Facades\DB::table('productos')->exists()) {
+            $this->command?->info('La base de datos ya contiene productos. Se omite la carga de ejemplos para no sobrescribir datos.');
+            return;
+        }
+
         $excel = database_path('seeders/ExelMultiSede.xlsx');
         if (! is_file($excel)) {
             $this->command?->warn('ExelMultiSede.xlsx no encontrado; seeder omitido.');
