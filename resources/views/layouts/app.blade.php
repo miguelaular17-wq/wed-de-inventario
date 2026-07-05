7152bcfba0312439e4f45ef2ff8c81bd8cad3fb7
Miguel Aular <miguelaular17@gmail.com>
2026-07-05 07:08:26 -0400

    fix: forzar recarga de cache css para paginacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/app.css?v={{ filemtime(public_path('css/app.css')) }}">
     @if(config('inventario.tutorial_enabled'))
     <link rel="stylesheet" href="/css/onboarding-tour.css">
     @endif
